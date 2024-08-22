46ab668894b1a0f3f040886d0c5318fd2541c552
Pedro Souza <pedro.ads510@gmail.com>
2024-08-22 12:48:06 -0300

    starting first route

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/pedrosouza458/go-open-graph-scraper-api
 
 go 1.22.5
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/pedrosouza458/go-open-graph-scraper v0.0.0-20240822151015-64d5728e7d63 // indirect
+	golang.org/x/net v0.28.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,24 +1,58 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	"strings"
+
+	"github.com/pedrosouza458/go-open-graph-scraper/pkg/scraper"
 )
 
 func main() {
-	http.HandleFunc("/websites/", websiteHandler)
+	http.HandleFunc("/websites", websiteHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
-func websiteHandler(w http.ResponseWriter, r *http.Request) {
-	// Extract the user ID from the URL path
-	path := strings.TrimPrefix(r.URL.Path, "/websites/")
-	if path == "" {
-		http.Error(w, "User ID not provided", http.StatusBadRequest)
-		return
-	}
+type Response struct {
+	Logo       string `json:"logo"`
+	StatucCode int    `json:"statusCode"`
+}
 
-	// Respond with the user ID
-	fmt.Fprintf(w, "User ID: %s", path)
+func websiteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	url := r.URL.Query().Get("url")
+	if url != "" {
+		logo, err := scraper.GetWebsiteLogo(url)
+		if err != nil {
+			fmt.Fprintf(w, "Error fetching logo: %v\n", err)
+			return
+		}
+		name, err := scraper.GetWebsiteName(url)
+		if err != nil {
+			fmt.Fprintf(w, "Error fetching name: %v\n", err)
+			return
+		}
+		img, err := scraper.GetWebsiteImg(url)
+		if err != nil {
+			fmt.Fprintf(w, "Error fetching img: %v\n", err)
+			return
+		}
+		title, err := scraper.GetWebsitePageTitle(url)
+		if err != nil {
+			fmt.Fprintf(w, "Error fetching website title: %v\n", err)
+			return
+		}
+		description, err := scraper.GetWebsiteDescription(url)
+		if err != nil {
+			fmt.Fprintf(w, "Error fetching description: %v\n", err)
+			return
+		}
+		json.NewEncoder(w).Encode(map[string]string{
+			"Logo":         logo,
+			"Name":         name,
+			"Img":          img,
+			"Title":        title,
+			"Descpription": description,
+		})
+	}
 }
